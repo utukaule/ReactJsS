9572b7a8440672c17b8379505c7223ae343c2766
Utkatsh Kaule <utkarshkaule7@gmail.com>
2023-01-03 09:16:38 +0530

    React js Basics
    Props, components

---
+++ myapp/src/App.jsx
@@ -0,0 +1,17 @@
+import Myheading from "./components/Myheading";
+import { Myheading2, Myheading3 } from "./components/Myheading"
+
+
+function App() {
+    return (
+
+        <div>
+            <Myheading  name ="utkarsh" text="12341234" />
+            <Myheading2 />
+            <Myheading3 />
+            <h1>Welcome to page one</h1>
+        </div>
+
+    )
+}
+export default App;
