b7cf5657c86532de1db0c95a9b59514cba128b7a
Utkatsh Kaule <utkarshkaule7@gmail.com>
2023-03-02 23:49:12 +0530

    Basic of usestate and useEffect
    Basic of usestate and useEffect

--- basicapp/src/App.js
+++ basicapp/src/App.js
@@ -3,6 +3,8 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Product from "./components/Product";
+import './styles/App.scss';
 import './styles/header.scss';
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About/>} />
           <Route path="/contact" element={<Contact/>}/>
+          <Route path="/product" element={<Product/>}/>
         </Routes>
       </Router>
     </>
