87ed635e0bfcd4cc8e611ea05d1e35c424f120c7
Utkatsh Kaule <utkarshkaule7@gmail.com>
2023-08-21 23:28:20 +0530

    news API data fetching in process

--- newsappcwh2/src/App.jsx
+++ newsappcwh2/src/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import News from './components/News'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        hihih
+        <Navbar/>
+        <News/>
       </div>
     )
   }
---
+++ newsappcwh2/src/components/News.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export default class News extends Component {
+  render() {
+    return (
+      <div>
+        sdkfksdf
+        <NewsItem/>
+      </div>
+    )
+  }
+}
+
