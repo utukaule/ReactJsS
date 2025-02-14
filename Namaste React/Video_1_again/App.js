dcf933dde892c47b37b40b11c7f1707833868ffb
Utkatsh Kaule <utkarshkaule7@gmail.com>
2025-02-14 10:08:30 +0530

    basic

--- Namaste React/Video_1_again/App.js
+++ Namaste React/Video_1_again/App.js
@@ -1,32 +1,58 @@
-// NOTE: while creating element using createElement react is not creating HTML, it is actually creating 
+// NOTE: while creating element using createElement react is not creating HTML, it is actually creating
 // object.
-// when it is rendeing on to the DOM eg.root.render(parent) it converts itself into the HTML and puts 
+// when it is rendeing on to the DOM eg.root.render(parent) it converts itself into the HTML and puts
 // itself into the element.
 
-
 // nested React
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    [
-        React.createElement("h1", {}, "Nested H1 Heading"),
-        React.createElement("h2", {}, "Nested H2 Heading"),
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-    ]
-  )
-);
-console.log(parent)
+const Title = () =>{
+    return <>
+        <h2> this is title</h2>
+    </>
+}   
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello React");
+const title = (
+    <h1> this is title</h1>
+)
 
+const number = 10;
+
+const Heading = () =>{
+    return <>
+        {number}{title}
+        <h1> this is heading</h1>
+    </>
+}
+
+
+// const heading = React.createElement('h1',{},'this is heading')
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Heading/>);
 
+// JSX heading
+// const jsxHeading = <h1>this is JSX code</h1>
 
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     [
+//         React.createElement("h1", {}, "Nested H1 Heading"),
+//         React.createElement("h2", {}, "Nested H2 Heading"),
 
+//     ]
+//   )
+// );
+// console.log(parent)
+
+// const heading = React.createElement("h1", { id: "heading" }, "Hello React");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
 
 // const heading = React.createElement('h1',{},"Hello React")
 
@@ -38,4 +64,4 @@ root.render(parent);
 // heading.innerHTML = 'this is heading';
 
 // const root = document.getElementById('root')
-// root.append(heading )
+// root.append(heading)
